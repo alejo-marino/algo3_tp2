62c93611b431825d4538d161a0842ca0b753697c
jm-velazquez <jvelazquez@fi.uba.ar>
2021-07-12 20:22:49 -0300

    Clase Batallon pasa los tests

---
+++ src/main/test/edu/fiuba/algo3/Modelo/Batallon.java
@@ -0,0 +1,41 @@
+package edu.fiuba.algo3.modelo;
+
+public class Batallon {
+
+    private int ejercitos;
+
+    public Batallon() {
+        ejercitos = 1;
+    }
+
+    public int getEjercitos() {
+        return ejercitos;
+    }
+
+    public int getEjercitosParaAtacar() {
+        if (ejercitos - 1 < 1) {
+            throw new EjercitosInsuficientesException();
+        }
+        if (ejercitos >= 4) {
+            return 3;
+        } else {
+            return ejercitos - 1;
+        }
+
+    }
+
+    public void agregarEjercitos(int cantidadEjercitos) {
+        ejercitos = ejercitos + cantidadEjercitos;
+    }
+
+    public boolean tengoEjercitos() {
+        return ejercitos > 0;
+    }
+
+    public void disminuirEjercitos(int cantidadEjercitos) {
+        ejercitos = ejercitos - cantidadEjercitos;
+        if (ejercitos < 0) {
+            ejercitos = 0;
+        }
+    }
+}
