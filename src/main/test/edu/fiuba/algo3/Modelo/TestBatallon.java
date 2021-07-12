5f46cc242db69451bf4c73d2aa14a8cf4fcc0d89
jm-velazquez <jvelazquez@fi.uba.ar>
2021-07-12 20:37:20 -0300

    Test03 arreglado Y Clases Movidas a la carpeta adecuada

--- src/main/test/edu/fiuba/algo3/Modelo/TestBatallon.java
+++ src/main/test/edu/fiuba/algo3/Modelo/TestBatallon.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.modelo.excepciones.EjercitosInsuficientesException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
