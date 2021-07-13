63718f3a2dfc191ac7ac52deeba3af7a86d2678a
Juani <juanicolombo@icloud.com>
2021-07-12 21:53:19 -0300

    creo ventanas iniciales

--- src/main/java/edu/fiuba/algo3/Vista/App.java
+++ src/main/java/edu/fiuba/algo3/Vista/App.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.Vista;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
@@ -13,23 +14,50 @@ import javafx.stage.Stage;
 /**
  * JavaFX App
  */
-public class App extends Application {
-
+public class App extends Application{
+    Stage window;
     Button button;
+
+    Scene escenaCantidadJugadores, escenaInicial;
+
     @Override
     public void start(Stage stage) {
-        stage.setTitle("ALGO-TEG");
+        window = stage;
+    // Escena 1
+
 
         button = new Button("Empezar Juego");
+        button.setOnAction(e -> window.setScene(escenaCantidadJugadores));
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
+        escenaInicial = new Scene(layout, 300, 300);
 
-        Scene scene = new Scene(layout, 300, 300);
-        stage.setScene(scene);
-        stage.show();
+    //Escena 2
+
+        Button button2 = new Button("Cantidad Jugadores");
+        button2.setOnAction(e ->AlertBox.display("Cantidad De Jugadores", "Se permite un minimo de 2 y maximo de 6 jugadores."));
+
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        escenaCantidadJugadores = new Scene(layout2, 300, 300);
+
+        // Config ventana inicial
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            cerrarPrograma();
+        });
+        window.setScene(escenaInicial);
+        window.setTitle("ALGO-TEG");
+        window.show();
     }
 
+    public void cerrarPrograma() {
+        Boolean respuesta = ConfirmBox.display("Desea Salir?");
+        if (respuesta) {
+            window.close();
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
