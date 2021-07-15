572850fbbea2e3d6c89a156e068f8570a30550e1
jm-velazquez <jvelazquez@fi.uba.ar>
2021-07-15 20:57:57 -0300

    Parser incompleto

--- src/main/java/edu/fiuba/algo3/Modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/Modelo/Pais.java
@@ -1,8 +1,12 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.modelo.excepciones.AtaqueAPaisNoLimitrofeException;
+import edu.fiuba.algo3.modelo.excepciones.AtaqueAPaisPropioException;
+import edu.fiuba.algo3.modelo.excepciones.AtaqueConPaisAjenoException;
 import edu.fiuba.algo3.modelo.excepciones.ReforzarPaisAjenoError;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Pais {
 
@@ -34,7 +38,6 @@ public class Pais {
         return duenio;
     }
 
-
     public void reforzar(Jugador jugador, int cantidadEjercitos) {
         if (jugador != duenio) {
             throw new ReforzarPaisAjenoError("Este pais no te pertenece");
@@ -55,22 +58,63 @@ public class Pais {
         return nombre;
     }
 
-
-
     public boolean esAliado(Pais pais) {
         return this.duenio == pais.getPaisOcupadoPor();
     }
 
-    public void conquistar(Pais defensor){
-        defensor.duenio = this.duenio;
-        defensor.reforzar(this.duenio, 1);
+    public void conquistarFinal(Pais conquistado){
+        conquistado.duenio = this.duenio;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Cuantas piezas queres pasar a " + conquistado.toString() + "?");
+        int cantEjercitosAMover = input.nextInt();
+        int cantEjercitos = this.batallon.getEjercitos();
+        if (cantEjercitosAMover < 1 || cantEjercitosAMover > (cantEjercitos - 1)) {
+            System.out.println("La cantidad a movilizar es invalida.");
+            this.conquistar(conquistado);
+            return;
+        }
+        this.disminuirEjercitos(cantEjercitosAMover);
+        conquistado.reforzar(this.duenio, cantEjercitosAMover);
+    }
+
+    public void conquistar(Pais conquistado){
+        conquistado.duenio = this.duenio;
+        conquistado.reforzar(this.duenio, 1);
         this.disminuirEjercitos(1);
     }
+
     public void hacerLimitrofe(Pais pais) {
         this.paisesLimitrofes.add(pais);
     }
 
-    public boolean esLimitrofeCon(Pais pais) {
-        return this.paisesLimitrofes.contains(pais);
+    public void esLimitrofeCon(Pais pais) {
+        if (!this.paisesLimitrofes.contains(pais)) {
+            throw new AtaqueAPaisNoLimitrofeException(this.toString() + " y " + pais.toString() + " no son limitrofes.");
+        }
+    }
+
+    public void verificarAtaque(Jugador jugador, Pais defensor) {
+        this.esDuenioPais(jugador);
+        this.esLimitrofeCon(defensor);
+        this.esDuenioPaisAtacado(defensor);
+
+    }
+
+    private void esDuenioPaisAtacado(Pais defensor) {
+        if (this.esAliado(defensor)) {
+            throw new AtaqueAPaisPropioException(defensor.toString()+ " te pertenece, no podes atacarlo.");
+        }
+    }
+
+    private void esDuenioPais(Jugador jugador) {
+        if (jugador != duenio) {
+            throw new AtaqueConPaisAjenoException("Este país no te pertenece");
+        }
+    }
+
+    public void serConquistadoPor(Pais atacante) {
+        if (!this.tengoEjercitos()) {
+            atacante.conquistar(this);
+        }
     }
 }
---
+++ src/main/java/edu/fiuba/algo3/Modelo/excepciones/ArchivoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo.excepciones;
+
+public class ArchivoNoEncontradoException extends RuntimeException {
+    public ArchivoNoEncontradoException(String msg) {
+        super((msg));
+    }
+}
