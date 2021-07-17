a22d9ad179c1e8cc429a4a88e69235a215f39dd3
Juani <juanicolombo@icloud.com>
2021-07-17 18:42:40 -0300

    creo menu inicial scene builder

--- src/main/java/edu/fiuba/algo3/Vista/App.java
+++ src/main/java/edu/fiuba/algo3/Vista/App.java
@@ -6,7 +6,9 @@ import edu.fiuba.algo3.modelo.excepciones.CantidadErroneaDeJugadoresError;
 import javafx.application.Application;
 //import javafx.event.EventHandler;
 //import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.Scene;
 //import javafx.scene.control.Label;
@@ -22,7 +24,9 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 /**
  * JavaFX App
@@ -34,61 +38,21 @@ public class App extends Application{
     Scene escenaCantidadJugadores, escenaInicial;
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
+        URL url = new File("src/main/resources/vistas/menuInicio.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
 
 
-        window = stage;
-    // Escena 1
-        final ImageView imagen = new ImageView("imagenes/mapaMenuInicio.jpeg");
+        Scene scene = new Scene(root);
+        stage.setOnCloseRequest(e -> cerrarPrograma());
+        stage.setScene(scene);
+        stage.show();
 
-
-        button = new Button("Empezar Juego");
-        button.setOnAction(e -> window.setScene(escenaCantidadJugadores));
-
-        StackPane layout = new StackPane();
-        layout.getChildren().addAll(imagen,button);
-
-        Media mediaMenuInicio = new Media(new File("src/main/resources/sonidos/Game-Menu.mp3").toURI().toString());
-        MediaPlayer menuinicio = new MediaPlayer(mediaMenuInicio);
-        menuinicio.play();
-
-        escenaInicial = new Scene(layout, 420, 420);
-        imagen.fitHeightProperty().bind(escenaInicial.heightProperty());
-        imagen.fitWidthProperty().bind(escenaInicial.widthProperty());
-
-    //Escena 2
-
-        TextField input = new TextField("Cantidad Jugadores");
-        input.setOnMouseClicked(e -> input.clear());
-        Button button2 = new Button("Empezar");
-        button2.setOnAction(e ->{
-            try {
-                Juego juegoNuevo = new Juego(Integer.parseInt(input.getText()));
-                SeleccionNombreJugadores.display("Nombre jugadores", Integer.parseInt(input.getText()));
-
-            }catch (CantidadErroneaDeJugadoresError error){
-                AlertBox.display("Error", error.getMessage());
-            }
-        });
-
-        VBox layout2 = new VBox(10);
-        layout2.setPadding(new Insets(20,20,20,20));
-        layout2.getChildren().addAll(input,button2);
-        escenaCantidadJugadores = new Scene(layout2, 300, 300);
-
-        // Config ventana inicial
-        window.setOnCloseRequest(e -> {
-            e.consume();
-            cerrarPrograma();
-        });
-        window.setScene(escenaInicial);
-        window.setTitle("ALGO-TEG");
-        window.show();
     }
 
     public void cerrarPrograma() {
         Boolean respuesta = ConfirmBox.display("Desea Salir?");
-        if (respuesta) {
+        if (!respuesta) {
             window.close();
         }
     }
