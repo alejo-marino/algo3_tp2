5b5e5732d382e0ad7dcc4750890cae6b073346a5
jm-velazquez <jvelazquez@fi.uba.ar>
2021-07-19 18:30:02 -0300

    Changes by alejo

---
+++ src/main/java/edu/fiuba/algo3/modelo/excepciones/ReagrupeInvalidoException.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo.excepciones;
+
+public class ReagrupeInvalidoException extends RuntimeException {
+    public ReagrupeInvalidoException(String msg) {
+        super((msg));
+    }
+}
